7211c8b0a6a65da45718c883b70ca4cfe3cc0da8
3xt1ncion <meln1kbtw@gmail.com>
2023-10-10 00:52:27 +0300

    6

--- app/src/main/java/com/example/handhtest/MainActivity.kt
+++ app/src/main/java/com/example/handhtest/MainActivity.kt
@@ -1,6 +1,46 @@
 package com.example.handhtest
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fightButton: Button
+    private lateinit var textMessageView: TextView
 
+    private var isGamePlayed: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fightButton = findViewById(R.id.fightButton)
+        textMessageView = findViewById(R.id.textMessageView)
+
+        fightButton.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivityForResult(intent, GAME_ACTIVITY_REQUEST_CODE)
+        }
+
+        if (isGamePlayed) {
+            textMessageView.text = "Get ready to rise again! In Mobile Souls, death is just the beginning. Every defeat is a chance to learn, to adapt, and to emerge stronger. Embrace the challenge, test your determination, and conquer the unforgiving world that awaits. Forge your own path, overcome insurmountable odds, and discover the sweet taste of victory that comes after countless failures. Are you ready to face the abyss once more?"
+        } else {
+            textMessageView.text = "Welcome to the Mobile Souls, where dangers, dark secrets, and powerful enemies await you. Prepare for a treacherous journey, where your survival relies on battles, skill improvement, and overcoming challenges. Guide your character through gloomy locations and uncover ancient mysteries. Are you ready for this challenge? Mobile Souls awaits you."
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GAME_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                isGamePlayed = true
+            }
+        }
+    }
+
+    companion object {
+        const val GAME_ACTIVITY_REQUEST_CODE = 1
+    }
 }
